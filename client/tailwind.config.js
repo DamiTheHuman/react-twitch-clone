58c58cf40f39269bf81ff7d8de418276f2631bc5
Dami Adesoye <dami98haniel@hotmail.com>
2021-03-14 17:04:17 +0000

    Initial Commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,40 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: colors.black,
+      primaryV: colors.white,
+      secondary: "#3b82f6",
+      secondaryV: "#bfdbfe",
+      dark: "#292929",
+      darkPrimary: colors.white,
+      darkPrimaryV: colors.black,
+      darkSecondary: "#bfdbfe",
+      darkSecondaryV: "#3b82f6",
+      darkHeader: "#1f1f1f",
+      success: "#22bb33",
+      warning: "#ffc107",
+      danger: "#dc3545",
+      muted: "#6c757d",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+    },
+    zIndex: {
+      neg20: -20,
+      neg10: -10,
+      0: 0,
+      10: 10,
+      20: 20,
+      auto: "auto",
+    },
+  },
+  Vs: {
+    extend: {},
+  },
+  plugins: [],
+};
