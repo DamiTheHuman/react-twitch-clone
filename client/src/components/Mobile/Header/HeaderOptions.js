dd3cd19c1415cfc4edc1028e40a8e68a09dfee6f
Dami Adesoye <dami98haniel@hotmail.com>
2021-03-17 17:34:54 +0000

    Updated the media query style for the StreamsIndex Live Stream Card, Implemented a clickable options for the Menu Navigation bar

---
+++ client/src/components/Mobile/Header/HeaderOptions.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./HeaderOptions.css";
+
+class HeaderOptions extends React.Component {
+  componentDidMount() {
+    document.addEventListener("click", this.handleClickOutside, true);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("click", this.handleClickOutside, true);
+  }
+  /** Handles the event when a user clicks outside the relement*/
+  handleClickOutside = (event) => {
+    const domNode = ReactDOM.findDOMNode(this);
+    if (!domNode || !domNode.contains(event.target)) {
+      this.props.onClickOutside();
+    }
+  };
+  render() {
+    return (
+      <div className="options-dialogue flex flex-col w-80 absolute bg-light block shadow-xl text-primary rounded text-sm px-2">
+        <div className="py-2.5">Privacy Policy</div>
+        <div className="py-2.5">Cookie Policy</div>
+        <div className="py-2.5 border-b border-gray-300">Terms Of Service</div>
+        <div className="py-2.5">Toggle Dark Theme</div>
+      </div>
+    );
+  }
+}
+
+export default HeaderOptions;
--- client/src/components/Mobile/Header/MiniHeader.js
+++ client/src/components/Mobile/Header/MiniHeader.js
@@ -3,26 +3,55 @@ import AlertIcon from "mdi-react/DotsHorizontalIcon";
 import ContentCopyIcon from "mdi-react/ContentCopyIcon";
 import SearchIcon from "mdi-react/SearchIcon";
 import TwitchLogo from "../../Icon/TwitchLogo";
+import HeaderOptions from "./HeaderOptions";
 
-const MiniHeader = () => {
-  return (
-    <div className="z-20 mini-header fixed top-0 w-full py-2 px-3 bg-white shadow-xl text-xl">
-      <div className="flex justify-between">
-        {/* First Column */}
-        <div className="flex space-x-6 items-center">
-          <TwitchLogo />
-          <ContentCopyIcon className="hover:text-primary" />
-        </div>
-        {/* Second Column */}
-        <div className="flex space-x-6 items-center">
-          <button className="hidden text-sm bg-primary text-white rounded px-2 py-1.5 font-bold">
-            Open in App
-          </button>
-          <SearchIcon color="#black" />
-          <AlertIcon />
+class MiniHeader extends React.Component {
+  state = { options: false };
+  /** Sets the value for the toggle option*/
+  toggleOptions = (value) => {
+    this.setState({ options: value });
+  };
+  /** Render the options menu*/
+  renderOptionsMenu = () => {
+    if (this.state.options) {
+      return (
+        <HeaderOptions
+          onClickOutside={() => {
+            this.toggleOptions(false);
+          }}
+        />
+      );
+    }
+  };
+  render() {
+    return (
+      <div className="z-20 mini-header fixed top-0 w-full py-2 px-3 bg-white shadow-xl text-xl">
+        <div className="flex justify-between">
+          {/* First Column */}
+          <div className="flex space-x-6 items-center">
+            <TwitchLogo />
+            <ContentCopyIcon className="hover:text-primary" />
+          </div>
+          {/* Second Column */}
+          <div className="flex space-x-6 items-center">
+            <button className="hidden text-sm bg-primary text-white rounded px-2 py-1.5 font-bold">
+              Open in App
+            </button>
+            <SearchIcon color="#black" />
+            <div className="relative">
+              <button
+                onClick={() => {
+                  this.toggleOptions(true);
+                }}
+              >
+                <AlertIcon />
+              </button>
+              {this.renderOptionsMenu()}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default MiniHeader;
